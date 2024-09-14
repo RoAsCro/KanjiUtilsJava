c0445e44d6ab48af4090f91a88501b40fe11f267
Roland Crompton <rollycrompton@gmail.com>
2024-09-14 10:30:57 +0100

    Written, passed test for kanjiapi working properly

--- src/test/java/self/roashe/kanutils/backend/dao/KanjiApiUtilTest.java
+++ src/test/java/self/roashe/kanutils/backend/dao/KanjiApiUtilTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import self.roashe.kanutils.backend.dao.WebConnection.KanjiApiUtil;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +27,27 @@ public class KanjiApiUtilTest {
         assertThrows(IOException.class, () -> KanjiApiUtil.getKanji('k'));
     }
 
-    
+    @Test
+    void testThrowsWhenNotFoundKana() {
+        assertThrows(IOException.class, () -> KanjiApiUtil.getKanji('く'));
+    }
+
+    @Test
+    void testKanjiCorrect() throws IOException, InterruptedException {
+        // Kanji
+        assertEquals('角', KanjiApiUtil.getKanji('角').getKanji());
+
+        // Kun
+        assertEquals(List.of("かど", "つの"), KanjiApiUtil.getKanji('角').getKunReadings());
+
+        // On
+        assertEquals(List.of("カク"), KanjiApiUtil.getKanji('角').getOnReadings());
+
+        // English
+        assertEquals(List.of("angle", "antlers", "corner", "horn", "square"),
+                KanjiApiUtil.getKanji('角').getEnglish());
+
+
+    }
 
 }
