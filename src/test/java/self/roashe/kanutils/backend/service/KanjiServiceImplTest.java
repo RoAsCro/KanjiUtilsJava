0a5eae776f846ed54c6920489f9c46c45dc28237
Roland Crompton <rollycrompton@gmail.com>
2024-09-16 15:10:44 +0100

    Tests for adding kanji added and passed

--- src/test/java/self/roashe/kanutils/backend/service/KanjiServiceImplTest.java
+++ src/test/java/self/roashe/kanutils/backend/service/KanjiServiceImplTest.java
@@ -9,7 +9,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import self.roashe.kanutils.backend.TestApplicationConfiguration;
 import self.roashe.kanutils.backend.dao.KanjiDao;
+import self.roashe.kanutils.backend.dao.WebConnection.KanjiApiUtil;
+import self.roashe.kanutils.backend.model.Kanji;
 import self.roashe.kanutils.backend.service.IOExceptions.KanjiIOException;
+
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
@@ -39,7 +44,43 @@ class KanjiServiceImplTest {
         assertEquals(1, dao.getAllKanji().size());
     }
 
-    
+    @Test
+    public void testAddKanjiDataSet() throws KanjiIOException, IOException, InterruptedException {
+        this.service.addKanji("水");
+        List<Kanji> kanjiList = dao.getAllKanji();
+        assertEquals(1, kanjiList.size());
+        Kanji kanji = kanjiList.get(0);
+        assertEquals(KanjiApiUtil.getKanji('水'), kanji);
+    }
+
+    @Test
+    public void testAddKanjiMultiple() throws KanjiIOException, IOException, InterruptedException {
+        this.service.addKanji("水日");
+        List<Kanji> kanjiList = dao.getAllKanji();
+        assertEquals(2, kanjiList.size());
+
+        assertEquals(List.of(KanjiApiUtil.getKanji('水'),
+                KanjiApiUtil.getKanji('日')),
+                dao.getAllKanji());
+    }
+
+    @Test
+    public void testAddKanjiNoKanji() throws KanjiIOException {
+        this.service.addKanji("test");
+        List<Kanji> kanjiList = dao.getAllKanji();
+        assertEquals(0, kanjiList.size());
+    }
+
+    @Test
+    public void testAddKanjiMixedText() throws KanjiIOException, IOException, InterruptedException {
+        this.service.addKanji("mizuwater水ぞeくasd日あｓ");
+        List<Kanji> kanjiList = dao.getAllKanji();
+        assertEquals(2, kanjiList.size());
+
+        assertEquals(List.of(KanjiApiUtil.getKanji('水'),
+                        KanjiApiUtil.getKanji('日')),
+                dao.getAllKanji());
+    }
 
 
 }
